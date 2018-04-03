0166d8045752949068e5b336e870123c173dba60
luosader <greatsen@sina.com>
2018-04-03 18:18:28 +0800

    2

---
+++ app/wdata/controller/IndexController.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\wdata\controller;
+
+use cmf\controller\UserBaseController;
+use app\wdata\model\WdataModel;
+
+/**
+* 数据中心
+*/
+class IndexController extends UserBaseController
+{
+    public function index()
+    {
+        $param = $this->request->param();
+        $scModel = new WdataModel;
+
+        $where = [];
+        $list = $scModel->field('*')->where($where)->paginate(25);
+// dump($list->items());die;
+        $this->assign('list',$list->items());
+        $this->assign('pager',$list->appends($param)->render());
+        return $this->fetch(':index');
+    }
+
+    public function detail()
+    {
+        $id = $this->request->param();
+
+        $scModel = new WdataModel;
+        $post = $scModel->get($id);
+
+        $this->assign('post',$post);
+        return $this->fetch(':detail');
+    }
+}
